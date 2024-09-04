bb56c22b9149e4cf53c76e9a7479a547668cf973
Johnnson <af7er.g@gmail.com>
2024-09-04 22:20:18 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Fragment } from 'react';
+
+/* import React, { useRef, useEffect } from 'react'; */
+/* import Canvas from 'react-canvas'; */
 
 function App() {
   return (
+
+    <Fragment>
+
+function MyCanvas() {
+  const canvasRef = useRef(null);
+
+  return (
+    <div>
+      <Canvas ref={canvasRef} />
+    </div>
+  );
+}
+    </Fragment>
     <div className="App">
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
