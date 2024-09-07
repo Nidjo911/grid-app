6d44f2833aafa6fb613a608eda4bb71cb9864c9f
Johnnson <af7er.g@gmail.com>
2024-09-07 21:22:23 +0200

    canvas upgrade

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { Fragment } from 'react';
-import React, { useRef, useEffect } from 'react';
-import Canvas from 'react-canvas';
-
-/* import React, { useRef, useEffect } from 'react'; */
-/* import Canvas from 'react-canvas'; */
-
-
+import MyCanvas from './Canvas';
 
 function App() {
-
-  function MyCanvas() {
-    const canvasRef = useRef(null);
-
-    return (
-      <div>
-
-        <Canvas ref={canvasRef} />
-      </div>
-    );
-  };
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-
-    // Perform drawing operations here
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(10, 10, 100, 100);
-
-    /* ctx.fillStyle = 'blue';
-ctx.fillRect(10, 10, 100, 100); */
-  }, []);
-
-
   return (
 
     <Fragment>
+
+      <MyCanvas />
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
---
+++ src/Canvas.jsx
@@ -0,0 +1,24 @@
+import React, { useRef, useEffect } from 'react';
+import Canvas from 'react-canvas';
+
+export default function MyCanvas() {
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+
+        // Perform drawing operations here
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(10, 10, 100, 100);
+
+        /* ctx.fillStyle = 'blue';
+    ctx.fillRect(10, 10, 100, 100); */
+    }, []);
+    return (
+        <div>
+
+            <Canvas ref={canvasRef} />
+        </div>
+    );
+};
